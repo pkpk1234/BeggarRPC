6a520f7e06c5a0892e10868eb38961859af7200c
lijiaming <lijiaming1984@gmail.com>
2018-04-05 11:51:43 +0800

    add methods

---
+++ src/main/java/com/beggar/rpc/server/Server.java
@@ -0,0 +1,11 @@
+package com.beggar.rpc.server;
+
+/**
+ * @author 李佳明 https://github.com/pkpk1234
+ * @date 2018-04-05
+ */
+public interface Server {
+    void start();
+    void stop();
+    void isStarting();
+}
