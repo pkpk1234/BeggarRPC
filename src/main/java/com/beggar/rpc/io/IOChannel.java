879d00408cf02130b4504ada566b017338edd5e5
lijiaming <lijiaming1984@gmail.com>
2018-04-05 22:34:46 +0800

    add interface

--- src/main/java/com/beggar/rpc/io/IOChannel.java
+++ src/main/java/com/beggar/rpc/io/IOChannel.java
@@ -4,7 +4,7 @@ package com.beggar.rpc.io;
  * @author 李佳明 https://github.com/pkpk1234
  * @date 2018-04-05
  */
-public interface IOChannel {
+public interface IOChannel{
     int read(byte[] buffer,int idx,int len);
     void write(byte[] buffer,int idx,int len);
 }
