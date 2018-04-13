3477571d50d24e87e960ee1f1759beb49a991b8f
lijiaming <lijiaming1984@gmail.com>
2018-04-13 23:13:52 +0800

    update

---
+++ src/main/java/com/beggar/rpc/protocol/Message.java
@@ -0,0 +1,10 @@
+package com.beggar.rpc.protocol;
+
+/**
+ * @author 李佳明 https://github.com/pkpk1234
+ * @date 2018-04-13
+ */
+public class Message {
+    private MessageHeader header;
+    private byte[] body;
+}
